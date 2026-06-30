16946f96eddebc895904891cfb0765547ac7758e
the-kr-abhishek <kkumarabhishek4896@gmail.com>
2026-06-30 16:52:27 +0530

    Added load_env

--- main_app/main.py
+++ main_app/main.py
@@ -15,7 +15,8 @@ from groq import Groq
 from services.coaching.llm import LLMCoach
 from services.coaching.tts import TextToSpeech
 from services.coaching.voice_pipeline import VoicePipeline, autoplay_audio
-
+from dotenv import load_dotenv
+load_dotenv()
   
 def main():
     st.set_page_config(
